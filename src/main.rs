31001bb5ca3caed275e6bdccf552b95e87f5049e
Renz Frederick <d3nzus@gmail.com>
2026-09-07 22:53:30 +0800

    docs: update comment to main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use crate::scanner::Tokenizer;
 use crate::scanner::Token;
 
 fn main() {
-    //test
+    //test works, ignore the compiler warnings
     let source = "let x = 42 + 5";
     let tokenizer = Tokenizer::new(source);
     let tokens: Vec<Token> = tokenizer.collect();
