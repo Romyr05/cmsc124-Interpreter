dac649559117a289c19c32be32a1251c4ebdfa8d
romyr05 <romyrlopez123@gmail.com>
2026-09-14 00:19:49 +0800

    feat: added an argument parsing for testing purposes

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,20 @@ mod scanner;
 mod token_app;
 use crate::scanner::Tokenizer;
 use crate::token_app::Token;
+use std::fs;
 
 fn main() {
-    //test works, ignore the compiler warnings
-    let source = "let x = 42 + 5 button () == aa";
-    let tokenizer = Tokenizer::new(source);
+    // iterates and gets the file name
+    let path = std::env::args().skip(1).find(|arg| !arg.starts_with("--"));
+
+    let source = match path {
+        Some(path) => fs::read_to_string(&path).expect("could not read input file"),
+        
+        // For manual purposes (cargo run)
+        None => String::from("let x = 42 + 5 button () == aa"),
+    };
+
+    let tokenizer = Tokenizer::new(&source);
     let tokens: Vec<Token> = tokenizer.collect();
     for token in tokens {
         println!("{:?}", token);
