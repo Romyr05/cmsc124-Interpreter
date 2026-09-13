165c7ff05c7ae81ce9ea1842d124c9b4cd33803e
romyr05 <romyrlopez123@gmail.com>
2026-09-13 22:44:01 +0800

    refactor: restructured the token files

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,33 +1,5 @@
 // enum token with different types, kulang pa ni
-#[derive(Debug)]
-pub enum Token<'a> {
-    LeftParen(&'a str),
-    RightParen(&'a str),
-    Plus(&'a str),
-    Equal(&'a str),
-    Number(&'a str),
-    Word(&'a str),
-    WhiteSpace(&'a str),
-    Identifier(&'a str),
-    // keyword tokens below
-    Button(&'a str),
-    Box(&'a str),
-    Text(&'a str),
-    Image(&'a str),
-    Div(&'a str),
-    Paragraph(&'a str),
-    // attributes
-    ID_name(&'a str),
-    Class_name(&'a str),
-    //styling
-    Style_Align(&'a str),
-    Style_Pad(&'a str),
-    Style_Margin(&'a str),
-    Style_Height(&'a str),
-    Style_Width(&'a str),
-    Style_Color(&'a str),
-    Style_Border(&'a str),
-}
+use crate::token_app::Token;
 
 pub struct Tokenizer<'a> {
     source: &'a str,
