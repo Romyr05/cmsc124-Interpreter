29f04d482ffe221bc87fd46ce8d25d4046e752d5
Renz Frederick <d3nzus@gmail.com>
2026-09-19 22:42:26 +0800

    fix: testing expectations

---
+++ src/ast.rs
@@ -0,0 +1,17 @@
+use crate::token::Token;
+
+#[derive(Debug, Clone)]
+pub enum Value {
+    Number(f64),
+    Str(String), // owned, so Value needs no lifetime
+    Bool(bool),
+    Nil,
+}
+
+#[derive(Debug, Clone)]
+pub enum Expr<'a> {
+    Literal { value: Value },
+    Unary { operator: Token<'a>, right: Box<Expr<'a>> },
+    Binary { left: Box<Expr<'a>>, operator: Token<'a>, right: Box<Expr<'a>> },
+    Grouping { expression: Box<Expr<'a>> },
+}
